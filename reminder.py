20ba2f604e03f4883563fe63af4d3c75c7cc0585
JMcB <joel.mcbride1@live.com>
2021-08-06 17:22:19 +0100

    Fix repeated notification issue
    Access the lower level notification api, and use the same balloon tip for each notification, instead of a new one each time

--- reminder.py
+++ reminder.py
@@ -8,7 +8,7 @@ import webbrowser
 import winsound
 from pathlib import Path
 
-import plyer
+import plyer.platforms.win.libs.balloontip
 
 # interval in minutes
 interval = 20
@@ -41,40 +41,44 @@ autoraise = False
 notify_at_startup = False
 
 
-def notify():
+def notify(windows_balloon_tip):
     # open page
     webbrowser.open(str(page_path), autoraise=autoraise)
 
     # send windows notification (toast)
-    plyer.notification.notify(title='Take a break!',
+    windows_balloon_tip.notify(title='Take a break!',
                               message='Blink your eyes, move them around, '
                                       'and look at a distant '
                                       'object for 20 seconds.',
-                              app_name='Eye Break',
-                              app_icon=str(icon_path))
+                              app_name='Eye Break',)
 
     # play sound
     winsound.PlaySound(str(sound_path), winsound.SND_FILENAME)
 
 
-def pre_notify():
-    plyer.notification.notify(title='Eye break in 1 minute',
+def pre_notify(windows_balloon_tip):
+    windows_balloon_tip.notify(title='Eye break in 1 minute',
                               message='Take an eye break',
-                              app_name='Eye Break',
-                              app_icon=str(icon_path))
+                              app_name='Eye Break',)
 
 
 # mainloop
 def main():
+    bt = plyer.platforms.win.libs.balloontip(
+        title='Eye break reminders started',
+        app_name='Eye Break',
+        app_icon=str(icon_path)
+    )
+    
     if notify_at_startup:
-        notify()
+        notify(bt)
 
     while True:
         # count down if interval is appropriate, otherwise just wait
         if type(interval) == int and interval > 1:
             for i in range(interval, 0, -1):
                 if i == 1:
-                    pre_notify()
+                    pre_notify(bt)
 
                 print(i, end=',', flush=True)
                 time.sleep(minute)
@@ -82,7 +86,7 @@ def main():
             time.sleep(interval*minute)
 
         print('\nHave a break have a KitKat ®')
-        notify()
+        notify(bt)
 
 
 if __name__ == '__main__':
