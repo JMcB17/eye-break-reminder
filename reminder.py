b2b6967f955664327622905db9c50f3b845064ad
jmcb <joelsgp@protonmail.com>
2022-05-15 15:39:00 +0100

    Remove cross-platform support because linux has KDE RSIBreak

--- reminder.py
+++ reminder.py
@@ -5,11 +5,8 @@ import time
 import webbrowser
 from pathlib import Path
 
-WIN = (sys.platform == 'win32')
-
 import plyer
-if WIN:
-    import plyer.platforms.win.libs.balloontip
+from plyer.platforms.win.libs.balloontip import WindowsBalloonTip
 from playsound import playsound
 
 # todo: add UI for countdown etc.
@@ -56,49 +53,28 @@ log.addHandler(file_handler)
 log.addHandler(stream_handler)
 
 
-def notify(windows_balloon_tip=None):
+def notify(bt: WindowsBalloonTip):
     # open page
     webbrowser.open(str(page_path), autoraise=autoraise)
+    playsound(str(sound_path.resolve()), block=False)
 
-    # send windows notification
-    handler = plyer.notification
-    if WIN and windows_balloon_tip is not None:
-        handler = windows_balloon_tip
-
-    try:
-        handler.notify(
-            title='Take a break!',
-            message='Blink your eyes, move them around, and look at a distant '
-                    'object for 20 seconds.',
-            app_name=app_name
-        )
-    except Exception as error:
-        log.exception(error)
-
-    # play sound
-    try:
-        playsound(str(sound_path.resolve()), block=False)
-    except ValueError as error:
-        raise ValueError(
-            'Error, try this:\nsudo apt install python3-gst-1.0'
-            '\nFrom https://github.com/TaylorSMarks/playsound/issues'
-            '/16#issuecomment-658182306'
-        ) from error
+    bt.notify(
+        title='Take a break!',
+        message='Blink your eyes, move them around, and look at a distant '
+                'object for 20 seconds.',
+        app_name=app_name
+    )
 
 
-def pre_notify(windows_balloon_tip=None):
-    handler = plyer.notification
-    if WIN and windows_balloon_tip is not None:
-        handler = windows_balloon_tip
-
-    handler.notify(
+def pre_notify(bt: WindowsBalloonTip):
+    bt.notify(
         title='Eye break in 1 minute',
         message='Take an eye break',
         app_name=app_name
     )
 
 
-def countdown(interval, bt=None):
+def countdown(interval, bt: WindowsBalloonTip):
     # count down if interval is appropriate, otherwise just wait
     if isinstance(interval, int) and interval > 1:
         for i in range(interval, 0, -1):
@@ -142,14 +118,12 @@ def resume_interval(
 
 
 def main_not_caught():
-    bt = None
-    if WIN:
-        bt = plyer.platforms.win.libs.balloontip.WindowsBalloonTip(
-            title='Eye break reminders started',
-            message=f'Reminders every {reminder_interval} minutes',
-            app_name=app_name,
-            app_icon=str(icon_path)
-        )
+    bt = WindowsBalloonTip(
+        title='Eye break reminders started',
+        message=f'Reminders every {reminder_interval} minutes',
+        app_name=app_name,
+        app_icon=str(icon_path)
+    )
     
     if notify_at_startup:
         notify(bt)
